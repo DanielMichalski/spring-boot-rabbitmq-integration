ce45d974e204f3ec82806ddc8d2e0206e734915f
Jhon Baron <jhon.baron@zabud.com.co>
2020-11-25 16:44:46 -0500

    added: lombok to reduce pojo classes verbose-mode.

--- rabbitmq-receiver/src/main/java/com/danielmichalski/rabbitmqreceiver/listener/MessageReceiverListener.java
+++ rabbitmq-receiver/src/main/java/com/danielmichalski/rabbitmqreceiver/listener/MessageReceiverListener.java
@@ -1,21 +1,25 @@
 package com.danielmichalski.rabbitmqreceiver.listener;
 
 import com.danielmichalski.rabbitmqreceiver.model.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
+@AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class MessageReceiverListener {
 
-    private static final Logger logger = LoggerFactory.getLogger(MessageReceiverListener.class);
 
     public static final String ROUTING_KEY = "queue";
 
     @RabbitListener(queues = ROUTING_KEY)
     public void listenerMessage(UserDto user) {
-        logger.info("Received user from the RabbitMQ: {}", user);
+        log.info("Received user from the RabbitMQ: {}", user);
     }
 
 }
